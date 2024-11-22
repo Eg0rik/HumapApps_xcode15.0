cf7133ac7d2788671057b0b7858b269a8d462f0a
Egor <eg887or@gmail.com>
2024-11-22 11:03:08 +0300

    first commit

---
+++ Task_xcode15.0/Task_xcode15.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task_xcode15.0
+//
+//  Created by MAC on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
